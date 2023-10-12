e89d3b1947da6038683219a4f9068fa72d00cfe3
Yağmur K <yagmurrttk@gmail.com>
2023-10-12 16:43:04 +0300

    Ders 27 Sonu

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
         public ActionResult Index()
         {
-            return View();
+            // var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            return View(by);
+        }
+        
+        public ActionResult BlogDetay(int id)
+        {
+            // var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.BlogID == id).ToList();
+            return View(by);
         }
     }
 }
--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
+
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.Take(10).ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.ToList();
+            return PartialView(deger);
         }
     }
 }
